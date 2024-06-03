9e6968fc21c5991fe967daac3ccd2e344b9bdd86
Myst33d <notmyst33d@gmail.com>
2024-06-03 14:52:04 +0600

    Split prefix for thumbnails and content files

--- indexgen.py
+++ indexgen.py
@@ -6,6 +6,7 @@ from urllib.parse import quote
 
 directory = sys.argv[1]
 prefix = sys.argv[2] if sys.argv[2] != "local" else ""
+thumb_prefix = sys.argv[2] if sys.argv[2] != "local" else ""
 content = ""
 
 with open("template.html", "r") as f:
@@ -17,10 +18,10 @@ for file in reversed(files):
     content += f"<a href=\"{prefix}{quote(file)}\">"
     if file.endswith(".mp4"):
         file_gif = file.replace(".mp4", ".gif")
-        content += f"<img loading=\"lazy\" class=\"thumbnail\" src=\"{prefix}thumbs/{quote(file_gif)}\">"
+        content += f"<img loading=\"lazy\" class=\"thumbnail\" src=\"{thumb_prefix}thumbs/{quote(file_gif)}\">"
     elif file.endswith(".jpg") or file.endswith(".png") or file.endswith(".jpeg"):
         file_jpg = file.replace(".png", ".jpg").replace(".jpeg", ".jpg")
-        content += f"<img loading=\"lazy\" class=\"thumbnail\" src=\"{prefix}thumbs/{quote(file_jpg)}\">"
+        content += f"<img loading=\"lazy\" class=\"thumbnail\" src=\"{thumb_prefix}thumbs/{quote(file_jpg)}\">"
     content += "</a>"
 
 with open("index.html", "w") as f:
